e08c9174c9e85aeacb4bfd4ec21fc7fb62097cd8
harborned <d.harborne@outlook.com>
2018-06-23 19:39:47 +0100

    model tidy

--- models/cnn_1/CNN.py
+++ models/cnn_1/CNN.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 class SimpleCNN(object):
-	"""docstring for SimpleCNN"""
+	"""A simple CNN model implemented using the Tensorflow estimator API"""
 	def __init__(self, model_input_dim_height, model_input_dim_width, model_input_channels, n_classes, learning_rate = 0.001, dropout = 0.25, model_dir = "model_dir"):
 		super(SimpleCNN, self).__init__()
 		self.model_input_dim_height = model_input_dim_height
@@ -19,11 +19,8 @@ class SimpleCNN(object):
 		self.model = None
 		self.InitaliseModel(model_dir=model_dir)
 
-		from tensorflow.python.client import device_lib
-		print("detected devices:")
-		print(device_lib.list_local_devices())
-
-	
+		
+	### Required Model Functions
 	def InitaliseModel(self, model_dir="model_dir"):
 		self.model = tf.estimator.Estimator(self.model_fn,model_dir=model_dir)
 	
@@ -61,40 +58,40 @@ class SimpleCNN(object):
 		print("model is automatically loaded from 'model_dir'")
 
 
+	### Model Specific Functions
 	def BuildModel(self, x, model_input_dim_height, model_input_dim_width, model_input_channels, n_classes, dropout, reuse, is_training):
-		with tf.device('/gpu:0'):
-			# Define a scope for reusing the variables
-			with tf.variable_scope('ConvNet', reuse=reuse):
-				# TF Estimator input is a dict, in case of multiple inputs
-				# x = x_dict['images']
+		# Define a scope for reusing the variables
+		with tf.variable_scope('ConvNet', reuse=reuse):
+			# TF Estimator input is a dict, in case of multiple inputs
+			# x = x_dict['images']
 
 
-				# Incase of flat input vector reshape to match picture format [Height x Width x Channel]
-				# Tensor input become 4-D: [Batch Size, Height, Width, Channel]
-				x = tf.reshape(x, shape=[-1, model_input_dim_height, model_input_dim_width, model_input_channels])
+			# Incase of flat input vector reshape to match picture format [Height x Width x Channel]
+			# Tensor input become 4-D: [Batch Size, Height, Width, Channel]
+			x = tf.reshape(x, shape=[-1, model_input_dim_height, model_input_dim_width, model_input_channels])
 
-				# Convolution Layer with 32 filters and a kernel size of 5
-				conv1 = tf.layers.conv2d(x, 32, 5, activation=tf.nn.relu)
-				# Max Pooling (down-sampling) with strides of 2 and kernel size of 2
-				conv1 = tf.layers.max_pooling2d(conv1, 2, 2)
+			# Convolution Layer with 32 filters and a kernel size of 5
+			conv1 = tf.layers.conv2d(x, 32, 5, activation=tf.nn.relu)
+			# Max Pooling (down-sampling) with strides of 2 and kernel size of 2
+			conv1 = tf.layers.max_pooling2d(conv1, 2, 2)
 
-				# Convolution Layer with 64 filters and a kernel size of 3
-				conv2 = tf.layers.conv2d(conv1, 64, 3, activation=tf.nn.relu)
-				# Max Pooling (down-sampling) with strides of 2 and kernel size of 2
-				conv2 = tf.layers.max_pooling2d(conv2, 2, 2)
+			# Convolution Layer with 64 filters and a kernel size of 3
+			conv2 = tf.layers.conv2d(conv1, 64, 3, activation=tf.nn.relu)
+			# Max Pooling (down-sampling) with strides of 2 and kernel size of 2
+			conv2 = tf.layers.max_pooling2d(conv2, 2, 2)
 
-				# Flatten the data to a 1-D vector for the fully connected layer
-				fc1 = tf.contrib.layers.flatten(conv2)
+			# Flatten the data to a 1-D vector for the fully connected layer
+			fc1 = tf.contrib.layers.flatten(conv2)
 
-				# Fully connected layer
-				fc1 = tf.layers.dense(fc1, 1048)
-				# Apply Dropout (if is_training is False, dropout is not applied)
-				fc1 = tf.layers.dropout(fc1, rate=dropout, training=is_training)
+			# Fully connected layer
+			fc1 = tf.layers.dense(fc1, 1048)
+			# Apply Dropout (if is_training is False, dropout is not applied)
+			fc1 = tf.layers.dropout(fc1, rate=dropout, training=is_training)
 
-				# Output layer, class prediction
-				logits = tf.layers.dense(fc1, n_classes)
+			# Output layer, class prediction
+			logits = tf.layers.dense(fc1, n_classes)
 
-				return logits
+			return logits
 
 
    	def model_fn(self, features, labels, mode):
