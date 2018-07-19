548ee6b13263a7cf5a61668a332aee0167bcff9d
harborned <d.harborne@outlook.com>
2018-07-19 21:38:42 +0100

    additional options added to LIME explainer

---
+++ testing_folder/shap_test.py
@@ -0,0 +1,11 @@
+import shap
+import numpy as np
+
+# select a set of background examples to take an expectation over
+background = x_train[np.random.choice(x_train.shape[0], 100, replace=False)]
+
+# explain predictions of the model on three images
+e = shap.DeepExplainer(model, background)
+# ...or pass tensors directly
+# e = shap.DeepExplainer((model.layers[0].input, model.layers[-1].output), background)
+shap_values = e.shap_values(x_test[1:5])
