026b9d3d1178e19ad8aa6662aa2dfecadadc073a
harborned <d.harborne@outlook.com>
2018-08-31 17:26:28 +0100

    training stats update

--- models/inception_resnet_v2_imagenet/inception_resnet_v2_imagenet.py
+++ models/inception_resnet_v2_imagenet/inception_resnet_v2_imagenet.py
@@ -29,6 +29,8 @@ class InceptionResNetV2Imagenet(object):
         self.model_dir = model_dir
 
         # model specific variables
+        self.min_height = 139
+        self.min_width = 139
         # Training Parameters
 
         if ("learning_rate" in additional_args):
@@ -171,7 +173,7 @@ class InceptionResNetV2Imagenet(object):
         print("FetchAllVariableValues - not implemented")
 
 
-   def CheckInputDimensions(self,input_shape,min_height,min_width):
+    def CheckInputDimensions(self,input_shape,min_height,min_width):
         if(len(input_shape) == 4):
             image_shape = input_shape[1:]
         else:
