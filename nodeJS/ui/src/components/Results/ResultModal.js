1c0b64142449e19ed2fc65f5130393f90e0a15f9
harritaylor <taylorh23@cardiff.ac.uk>
2018-09-04 13:07:47 +0100

    Fixed typo in results modal

--- nodeJS/ui/src/components/Results/ResultModal.js
+++ nodeJS/ui/src/components/Results/ResultModal.js
@@ -1,4 +1,4 @@
-import {Image, Modal, Table} from "semantic-ui-react";
+import {Image, Modal, Table, Segment, Icon, Divider, Grid} from "semantic-ui-react";
 import React, { Component } from "react";
 
 
@@ -10,18 +10,32 @@ class ResultModal extends Component {
 
         console.log(this.props);
 
+        let correct;
+
+        if (this.props.results.ground_truth === this.props.results.prediction) {
+            correct = <Icon color='green' name='thumbs up' />
+        } else {
+            correct = <Icon color='red' name='thumbs down' />
+        }
+
         return (
             <Modal trigger={
                             <Image size='medium' src={"data:image/png;base64," + this.props.results.explanation_image}/>
                             }>
-                <Modal.Header>{this.props.results.model} trained on {this.props.results.dataset} dataset explained by
-                    {this.props.results.interpreter}</Modal.Header>
+                <Modal.Header>
+                    {this.props.results.model} trained on {this.props.results.dataset} dataset explained by&nbsp;
+                    {this.props.results.interpreter}
+                </Modal.Header>
                 <Modal.Content>
                     {/*<Segment padded compact>*/}
                     {/*<Header as='h3'>{this.state.data.model} trained on {this.state.data.dataset} dataset explained by {this.state.data.interpreter} &nbsp;<a><Icon name='down arrow'/></a> &nbsp;</Header>*/}
-                    {/*<strong>Timestamp: </strong> 17-Aug-2018 12:2:27 <br/><br/>*/}
-                    {/*<strong>Model Prediction:</strong>   Gun Wielder &nbsp; &nbsp;*/}
-                    {/*<strong>Ground Truth:</strong> Gun Wielder &nbsp; &nbsp;  <Icon color='green' name='thumbs up' />*/}
+
+                    <strong>Timestamp: </strong>{this.props.results.start_time}
+                    <Divider/>
+                    <strong>Model Prediction:</strong> {this.props.results.ground_truth} &nbsp;  {correct}<br/>
+                    <strong>Ground Truth:</strong> {this.props.results.prediction}
+
+                    <Divider/>
 
                     <Table basic='very' collapsing>
                         <Table.Header>
