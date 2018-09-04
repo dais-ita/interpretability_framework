6e9e97bfdf34ac9b815d6d9cd698d47debbe76bc
Dan Harborne <d.harborne@outlook.com>
2018-09-04 12:43:38 +0100

    model check update

--- models/model_check.py
+++ models/model_check.py
@@ -1,9 +1,13 @@
 import os
+import shutil 
+
+trained_model_dir = "trained_model_files"
+
 
 model_dir = "."
 model_folders = os.listdir(model_dir)
 
-ignore_file_folders = ['archived_depricated_models', 'utils', 'random_forest', 'model_check.py', 'download_models.py', 'models.json', 'keras_api_simple', 'conv_svm', 'svm']
+ignore_file_folders = ["trained_model_files", 'archived_depricated_models', 'utils', 'random_forest', 'model_check.py', 'download_models.py', 'models.json', 'keras_api_simple', 'conv_svm', 'svm']
 
 model_folders = [folder for folder in model_folders if folder not in ignore_file_folders]
 
@@ -23,6 +27,17 @@ for model_folder in model_folders:
 			dataset_model_path = os.path.join(saved_path,dataset+".h5")
 			if(os.path.exists(dataset_model_path)):
 				models_available[model_folder].append(dataset)
+
+				
+				#model
+				output_model_path = os.path.join(trained_model_dir,model_folder)
+				if(not os.path.exists(output_model_path)):
+					os.mkdir(output_model_path)
+
+				#dataset
+				output_dataset_path = os.path.join(output_model_path,dataset+".h5")
+				
+				shutil.copy(dataset_model_path,output_dataset_path)
 
 
 for key in models_available:
